460cdeab0660ed681af50ed934c58d05eb3e4f67
faruk786 <umarfaruk.786@gmail.com>
2026-09-18 21:30:12 +0530

    revert

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -11,7 +11,8 @@ export function Sidebar() {
   return (
     <aside
       className={cn(
-        "sticky top-14 hidden self-start h-[calc(100vh-3.5rem)] shrink-0 flex-col border-r border-border bg-sidebar transition-[width] duration-200 lg:flex w-64",
+        "sticky top-14 hidden h-[calc(100dvh-3.5rem)] shrink-0 flex-col border-r border-border bg-sidebar transition-[width] duration-200 lg:flex",
+        collapsed ? "w-16" : "w-64",
       )}
     >
       <div className="flex items-center justify-between px-3 py-3">
